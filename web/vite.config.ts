d28bbb50062a9a60d66efa137f9bbdd45af9c6b2
Diwakar Aryal <divakararyal2055@gmail.com>
2026-03-16 23:13:25 +0545

    Fix React Router 404 with vite-plugin-vercel

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
